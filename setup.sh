f2d65b1b012ce6e4ef12f39252847070790f0f5c
LLMOperations <161003835+LLMOperations@users.noreply.github.com>
2024-02-28 18:51:13 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo aion:aion@123 | chpasswd
+echo "[TASK 1] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+
